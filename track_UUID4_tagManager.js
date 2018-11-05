0f9a8b5774f650947c228986396d1e3cbd9ae640
Unknown <anselma.woerner@gmail.com>
2018-11-05 22:37:14 +0100

    code snippets to copy in tag manager

---
+++ track_UUID4_tagManager.js
@@ -0,0 +1,20 @@
+// Dimension to collect UUID4
+if (document.cookie.indexOf('browser_uuid_set=1') == -1) {
+	/* Set a cookie so we won't override the UUID we just set */
+  document.cookie = 'browser_uuid_set=1; expires=Fri, 01 Jan 2100 12:00:00 UTC; path=/';
+
+	function uuidv4() {
+ 	 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+   		 var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    	return v.toString(16);
+  		});
+	}
+}
+
+
+//Dimension to collect timestamp in utc ms
+function time_ms() {
+ /* Assign a timestamp to the utc_millisecs custom dimension */
+	var time_ms = new Date().getTime();
+  	return time_ms
+}
